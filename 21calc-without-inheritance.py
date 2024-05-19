1dcd0b641cda0fc3f1e6f9d0bb9163567e3b228b
Paul Ventura <j.paul.ventura@gmail.com>
2024-05-19 09:57:53 -0500

    Sum without inheritance

---
+++ 21calc-without-inheritance.py
@@ -0,0 +1,18 @@
+class Sum():
+  sum = 0
+  args = [0,1]
+
+  def __init__(self, args):
+    self.args[0] = args[0]
+    self.args[1] = args[1]
+    self.sum = self.args[0]+self.args[1]
+  
+  def response(self):
+    print("The response is: ")
+    return self.sum
+
+object_1 = Sum([12,13])
+
+print(object_1.args[0])
+print(object_1.args[1])
+print(object_1.response())
