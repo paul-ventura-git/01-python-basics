ad0fba482721cefac87bd85e5e48e80d55a2a8c4
Paul Ventura <j.paul.ventura@gmail.com>
2024-05-19 10:57:52 -0500

    04

--- 07lambda.py
+++ 07lambda.py
@@ -1,3 +1,13 @@
+x = lambda a : a + 10
+print(x(5))
+
+#############################################################
+
+x = lambda a, b : a * b
+print(x(5, 6))
+
+#############################################################
+
 def myfunc(n):
   return lambda a : a * n
 
@@ -6,3 +16,13 @@ mytripler = myfunc(3)
 
 print(mydoubler(11))
 print(mytripler(11))
+
+#############################################################
+
+lambda x: x + 1
+
+print ((lambda x: x + 1)(2))
+
+# (lambda x: x + 1)(2) = lambda 2: 2 + 1
+#                      = 2 + 1
+#                      = 3
---
+++ 22calc-with-lambda.py
@@ -0,0 +1,9 @@
+my_sum = lambda x, y : x + y
+my_diff = lambda x, y : x - y
+my_prod = lambda x, y : x * y
+my_div = lambda x, y : x / y
+
+print(my_sum(33,44))
+print(my_diff(33,44))
+print(my_prod(33,44))
+print(my_div(33,44))
