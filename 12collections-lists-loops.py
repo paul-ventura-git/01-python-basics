db7dd2a53ca1feb27f977502341ae3506f97ace1
Paul Ventura <j.paul.ventura@gmail.com>
2024-05-19 12:14:05 -0500

    06

--- 12collections-lists-loops.py
+++ 12collections-lists-loops.py
@@ -2,7 +2,18 @@ thislist = ["apple", "banana", "cherry"]
 for x in thislist:
   print(x)
 
-thislist = ["apple", "banana", "cherry"]
+# Referring to the index with FOR
+thislist = ["00", "01", "02"]
 for i in range(len(thislist)):
   print(thislist[i])
 
+# Referring to the index with WHILE
+thislist = ["apple", "banana", "cherry"]
+i = 0
+while i < len(thislist):
+  print(thislist[i])
+  i = i + 1
+
+# List Comprehension offers the shortest syntax for looping through lists:
+thislist = ["first", "second", "third"]
+[print(x) for x in thislist]
