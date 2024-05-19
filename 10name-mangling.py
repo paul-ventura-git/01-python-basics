59994435e6b4f8f933badc301e15361d8ddf28f9
Paul Ventura <j.paul.ventura@gmail.com>
2024-05-19 10:23:08 -0500

    03

--- 10name-mangling.py
+++ 10name-mangling.py
@@ -11,5 +11,12 @@ sample_instance = SampleClass("Hello!")
 print(vars(sample_instance))
 print(vars(SampleClass))
 
-print(sample_instance.__value)
-print(sample_instance.__method())
+# This internal behavior hides the names, creating the illusion of a private attribute or method.
+# However, they’re not strictly private. You can access them through their mangled names:
+
+print(sample_instance._SampleClass__value)
+print(sample_instance._SampleClass__method())
+
+# Name mangling is particularly useful when you want to ensure that a given attribute or method won’t get accidentally overwritten. 
+# It’s a way to avoid naming conflicts between classes or subclasses. 
+# It’s also useful to prevent subclasses from overriding methods that have been optimized for better performance.
