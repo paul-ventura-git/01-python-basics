ce3d7b1a32a6c72e798faf0a2a925d3dac43f3ad
Paul Ventura <j.paul.ventura@gmail.com>
2024-06-13 16:17:26 -0500

    03

--- 01loops-for.py
+++ 01loops-for.py
@@ -10,7 +10,7 @@ print("******************** for y in 'banana':")
 for y in "banana":
     print(y)
 
-# Print all the elements in the STRING until 'cherry' is found
+# Print all the elements in the STRING until 'cherry' is found (BREAK)
 print("******************** for z in fruits: if z == 'cherry': continue")
 fruits = ["apple", "banana", "cherry"]
 for z in fruits:
@@ -18,16 +18,24 @@ for z in fruits:
         continue
     print(z)
 
-print("#############################")
+# "in range" have a sequence like the indexes of arrays (starting from zero)
+# Print all the elements until number 6 (<6)
+print("******************** for x in range(6):")
 for x in range(6):
   print(x)
 
+# Print all the elements from 2 (1) until number 6 (5)
+print("******************** for x in range(2, 6):")
 for x in range(2, 6):
   print(x)
 
+# Print all the elements from 2 (1) until number 30 (29). Three by three
+print("******************** for x in range(2, 30, 3):")
 for x in range(2, 30, 3):
   print(x)
 
+# Print all the elements and break when value is met
+print("******************** for x in range(6): if x == 3: break")
 for x in range(6):
   if x == 3: break
   print(x)
