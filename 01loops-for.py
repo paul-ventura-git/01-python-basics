b814310871a788d330195956959104f78f5484ba
Paul Ventura <j.paul.ventura@gmail.com>
2024-06-13 16:06:00 -0500

    02

--- 01loops-for.py
+++ 01loops-for.py
@@ -1,19 +1,24 @@
 cars = ["audi", "chevrolet", "ford", "lexus"]
 
-# Print all the elements in the list
+# Print all the elements in the LIST
 print("******************** for x in cars:")
 for x in cars:
     print(x)
 
+# Print all the elements in the STRING
+print("******************** for y in 'banana':")
 for y in "banana":
     print(y)
 
+# Print all the elements in the STRING until 'cherry' is found
+print("******************** for z in fruits: if z == 'cherry': continue")
 fruits = ["apple", "banana", "cherry"]
 for z in fruits:
-    if z == "banana":
+    if z == "cherry":
         continue
-    print(x)
+    print(z)
 
+print("#############################")
 for x in range(6):
   print(x)
 
