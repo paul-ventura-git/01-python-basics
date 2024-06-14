2242c2bb5c3240251639282de9a9baa7cb9347ce
Paul Ventura <j.paul.ventura@gmail.com>
2024-06-13 19:54:50 -0500

    07

--- 07lambda.py
+++ 07lambda.py
@@ -37,3 +37,18 @@ print(adder (500, 500))
 #What a lambda returns #2
 x="some kind of a useless lambda"
 (lambda x : print(x))(x)
+
+#############################################################
+#A REGULAR FUNCTION
+def guru( funct, *args ):
+  funct( *args )
+def printer_one( arg ):
+  return print (arg)
+def printer_two( arg ):
+  print(arg)
+#CALL A REGULAR FUNCTION 
+guru( printer_one, 'printer 1 REGULAR CALL' )
+guru( printer_two, 'printer 2 REGULAR CALL \n' )
+#CALL A REGULAR FUNCTION THRU A LAMBDA
+guru(lambda: printer_one('printer 1 LAMBDA CALL'))
+guru(lambda: printer_two('printer 2 LAMBDA CALL'))
